7e8c067a2355cd5dbd403c838eb3744bf529d17f
Aniol Rodriguez <arodrigue96@gmail.com>
2024-09-20 00:17:54 +0200

    Create loginUser() with email and password validation if already existing && error message && save data && greeting

--- src/giveaways.ts
+++ src/giveaways.ts
@@ -3,3 +3,22 @@ import { programData, saveData } from "./storage.js";
 
 import { Giveaway } from "./types.js";
 import { askUserNewGiveawayData } from "./ui.js";
+
+// Task 1. Inicio de sesión
+export const loginUser = (email: string, password: string): void => {
+  const user = programData.users.find(
+    (user) => user.email === email && user.password === password
+  );
+
+  if (!user) {
+    console.log("Error: Usuario o contraseña incorrectos");
+    process.exit(1);
+  }
+
+  programData.userEmail = user.email;
+  programData.isAdmin = user.isAdmin;
+
+  saveData();
+
+  console.log(`Hola, ${user.name}!`);
+};
